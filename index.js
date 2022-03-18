00fa74033f0cd8d5eeaa5d5a4ded864d5272cbfd
Andy Antes <aantes@salesforce.com>
2022-03-18 09:52:42 -0400

    Initial file dump. Contains basic working AC Trie and AC Node classes, need unit tests

---
+++ index.js
@@ -0,0 +1,82 @@
+class ACNode {
+    constructor(data = null, parent=null, isWordNode=false) {
+        this.data = data;
+        this.parent = parent;
+        this.children = [] // List of ACNodes
+        this.failureLink = null; // Each ACNode should only have one failure link
+    }
+
+    hasChild(data) {
+        let result = false;
+        this.children.forEach(child => {
+            if (child.data === data) {
+                result = true;
+            }
+        });
+        return result;
+    }
+
+    getChild(data) {
+        // TODO: Handle failure to find
+        return this.children.find(child => child.data == data);
+    }
+
+    addChild(data, isWordNode=false) {
+        let child = new ACNode(data, this, isWordNode);
+        this.children.push(child);
+        return child;
+    }
+
+    printChildren() {
+        this.children.forEach(child => {
+            console.log(child.data);
+        })
+    }
+
+}
+
+class ACTrie {
+    constructor(wordList) {
+        this.root = new ACNode("");
+        let curr = this.root;
+        wordList.forEach(word => {
+            let isWordNode = false;
+            for (let i = 0; i < word.length; i++) {
+                let char = word[i];
+                if (i === word.length - 1) {
+                    isWordNode = true;
+                }
+                if (!curr.hasChild(char)) {
+                    let newChild = curr.addChild(char, isWordNode);
+                    curr = newChild;
+                } else {
+                    curr = curr.getChild(char);
+                }
+            }
+            curr = this.root;
+        })
+    }
+
+    printTree(root) {
+        if (root == null) {
+            return;
+        }
+        for (let i = 0; i < root.children.length; i++) {
+            console.log(root.children[i].data);
+        }
+        for (let i = 0; i < root.children.length; i++) {
+            this.printTree(root.children[i]);
+        }
+    }
+}
+
+/**
+ * If current node children does not have char,
+ * Add char to children, advance current to child, move on
+ * Else, move on to next letter, advance current
+ * to the child
+ */
+
+
+// Testing
+let ac = new ACTrie(['']);
