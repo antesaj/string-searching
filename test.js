403e13ffa84b26be7b8d6e3f809c6f4eceb121ac
Andy Antes <aantes@salesforce.com>
2022-03-18 11:24:58 -0400

    added a couple more tests around adding children

--- test.js
+++ test.js
@@ -19,4 +19,18 @@ test('ACNode defaults failureLink to null', () => {
 test('ACNode starts with no children nodes', () => {
     const node = new ACNode();
     expect(node.getChildren()).toEqual([]);
-})
+});
+
+test('addChild adds correct child', () => {
+    const parent = new ACNode();
+    const newChild = parent.addChild('a');
+    expect(parent.getChildren().length).toBe(1);
+    const child = parent.getChildren()[0];
+    expect(child).toEqual(newChild);
+});
+
+test('ACNode parent is set correctly when using addChild', () => {
+    const parent = new ACNode();
+    const child = parent.addChild('a');
+    expect(child.getParent()).toEqual(parent);
+});
